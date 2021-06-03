eb6dd3ba4d1140903582866bf69aa83715fd0f5f
austinbrot <abrotman@stanford.edu>
2021-06-03 14:34:21 -0700

    add weighted bce loss

--- src/loss.py
+++ src/loss.py
@@ -18,12 +18,40 @@ class DiceLoss(nn.Module):
         )
         return 1. - dsc
 
+class WeightedBCELoss(nn.Module):
+    
+    def __init__(self, pos_weight):
+        super(WeightedBCELoss, self).__init__()
+        self.pos_weight = pos_weight
+        self.bce = nn.BCELoss(reduction='none')
+
+    def forward(self, y, y_true):
+        loss = self.weight * y_true * self.bce(y)
+        return torch.sum(loss) / loss.shape[0]
+
 
 class DistanceMapLoss(nn.Module):
     def __init__(self, smooth: float = 1.0):
         super(DistanceMapLoss, self).__init__()
         self.smooth = smooth
         self.bce = nn.BCELoss(reduction='none')
+
+    def forward(self, x, y, distance_map):
+        assert x.size() == y.size()\
+            and y.size() == distance_map.size(),\
+            f'x shape {x.shape}, y shape {y.shape}, and map shape {map.shape} incompatible'
+
+        loss = self.bce(x, y)
+        loss *= distance_map + self.smooth
+        return torch.sum(loss) / loss.shape[0]
+
+
+class DMDiceLoss(nn.Module):
+    def __init__(self, smooth: float = 1.0):
+        super(DMDiceLoss, self).__init__()
+        self.smooth = smooth
+        self.bce = nn.BCELoss(reduction='none')
+        self.dsc_loss = DiceLoss()
 
     def forward(self, x, y, distance_map):
         assert x.size() == y.size()\
